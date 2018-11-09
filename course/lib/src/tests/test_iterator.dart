68642ccd226a98e64916c4fe9ee6b13dd7f12542
zorn <zorntsai@jetfuel.com.tw>
2018-11-09 18:31:28 +0800

    N

--- course/lib/src/tests/test_iterator.dart
+++ course/lib/src/tests/test_iterator.dart
@@ -1,6 +1,21 @@
 
 import 'package:test/test.dart';
 
+/*
+main() {
+  var iter = [1,5,10].iterator;
+  while(iter.moveNext()) {
+    print(iter.current);
+  }
+
+  var iterable = new Iterable.generate(3);
+  var iter2 = iterable.map((n) => n*2).iterator;
+  while(iter2.moveNext()) {
+    print(iter2.current);
+  }
+}
+ */
+
 
 class Process {
   int id;
