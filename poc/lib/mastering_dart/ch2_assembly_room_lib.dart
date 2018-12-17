ffdf36c5b68fca4e830b1278e900f09645db1a51
zorn <zorntsai@jetfuel.com.tw>
2018-12-17 18:05:29 +0800

    O

--- poc/lib/mastering_dart/ch2_assembly_room_lib.dart
+++ poc/lib/mastering_dart/ch2_assembly_room_lib.dart
@@ -1,30 +1,54 @@
+abstract class Car {
+  String get color;
+  set color(String color);
+}
 
+class PassengerCar extends Car {
+  static int counter = 0;
+  String toString()=> "PassengerCar $color";
+  
+  String get color => "Passenger color";
+  set color(String color) { 
+     color = color;    
 
-class AssemblyLine<E extends Car>{
-  List<E> _items = [];
-  add(E item){
-    _items.insert(0,item);
-  }
-  make(operation){
-    _items.forEach((E item){
-      operation(item);
-    });
-  }
-  contentInfo(){
-    print('length of _tiems: ${_items.length}');    
+    // 底下會出現 Maximum call stack size exceeded
+    // this.color = color
+ }
+}  
+
+class Truck extends Car{
+  String toString() => "Truck ";
+  String get color => "Truck color";
+  set color(String color) { 
+    color = color;
   }
 }
 
-abstract class Car{ String color;}
-class PassengerCar extends Car{ static int counter = 1 ;String toString()=>"Passenger Car ${counter++}";} 
-class Truck extends Car{String toString()=>"Truck";}
+class CarAssemblyLine<E extends Car>{  
+  List<E> items = [];  
+  void add(e)=> items.insert(0,e);
+  make(Function operate)=> items.map((c)=> operate(c));
+  log(operate)=> items.forEach((c)=>operate(c));
+}
 
-//Operation 類別實作 Function ( call ),表示這個類別可以當作function 用
-class Operation<E extends Car> implements Function{
-  final String opName;
-  Operation(this.opName);
-
+class Operation<E extends Car> implements Function {
   call(E car){
-    print('Make $opName on $car');
+    print('operate ${car}');
   }
 }
+
+/*
+void main()=> {
+   CarAssemblyLine line = CarAssemblyLine();
+  
+   var o1 = PassengerCar();
+  o1.color = "Blue";
+    var o2 = PassengerCar();
+  o2.color = "Pink";
+  
+   line.add(o1);
+   line.add(o2);
+  
+   line.log(Operation());
+};
+ */
